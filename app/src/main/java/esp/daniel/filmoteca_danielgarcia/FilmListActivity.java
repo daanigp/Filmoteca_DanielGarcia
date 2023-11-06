edb424ba196635a6668c4b559aa6ce9c0b63463a
daanigp <danielgarciapascual23@gmail.com>
2023-11-06 17:43:26 +0100

    Creación Menu
    Creación de menu, e implementación en la clase FilmListActivity

--- app/src/main/java/esp/daniel/filmoteca_danielgarcia/FilmListActivity.java
+++ app/src/main/java/esp/daniel/filmoteca_danielgarcia/FilmListActivity.java
@@ -1,8 +1,14 @@
 package esp.daniel.filmoteca_danielgarcia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class FilmListActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class FilmListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_film_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        int id = item.getItemId();
+        switch (id){
+            case R.id.itemAcercaDe:
+                Toast.makeText(getApplicationContext(), "Has pulsado sobre Acerca de.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(FilmListActivity.this, AboutActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.itemNuevaPeli:
+                Toast.makeText(getApplicationContext(), "Has pulsado sobre Añadir película.", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
