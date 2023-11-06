167c72d2246f7fc1fa33e72141fe6e1db2c31ca8
daanigp <danielgarciapascual23@gmail.com>
2023-11-06 17:21:03 +0100

    Actualización AboutActivity + FilmListActivity
    Creación de métodos para los botones: Soporte y sitio web.
    Creación FilmListActivity

--- app/src/main/java/esp/daniel/filmoteca_danielgarcia/AboutActivity.java
+++ app/src/main/java/esp/daniel/filmoteca_danielgarcia/AboutActivity.java
@@ -2,7 +2,11 @@ package esp.daniel.filmoteca_danielgarcia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +14,38 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_activity);
+
+        Button btnSitioWeb = (Button) findViewById(R.id.btnSitioWeb);
+        btnSitioWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com"));
+                //startActivity(intent);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        Button btnSoporte = (Button) findViewById(R.id.btnSoporte);
+        btnSoporte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Soporte Filmoteca.");
+                intent.putExtra(Intent.EXTRA_TEXT, "Texto del correo.");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"alumnotarde1@gmail.com"});
+                startActivity(intent);
+            }
+        });
+
+        Button btnVolver = (Button) findViewById(R.id.btnVolver);
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
