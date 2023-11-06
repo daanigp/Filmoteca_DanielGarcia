cfc88e3c6e2b471fb07231b55a06343df0e0bfc7
daanigp <danielgarciapascual23@gmail.com>
2023-11-06 18:06:59 +0100

    Creación clase Film
    Creación de la clase Film + arreglo en AboutActivity botón volver

--- app/src/main/java/esp/daniel/filmoteca_danielgarcia/AboutActivity.java
+++ app/src/main/java/esp/daniel/filmoteca_danielgarcia/AboutActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AboutActivity extends AppCompatActivity {
@@ -45,8 +46,7 @@ public class AboutActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Redirigiendote a la página principal.", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(AboutActivity.this, FilmListActivity.class);
-                startActivity(intent);
+                finish();
             }
         });
     }
---
+++ app/src/main/java/esp/daniel/filmoteca_danielgarcia/Film.java
@@ -0,0 +1,149 @@
+package esp.daniel.filmoteca_danielgarcia;
+
+public class Film {
+
+    //Tipos de formatos
+    //Almacena el índice en el que se encuentra el valor de cada formato en el recurso arrays.
+    public final static int FORMAT_DVD = 0;
+    public final static int FORMAT_BLURAY = 1;
+    public final static int FORMAT_DIGITAL = 2;
+
+    //Tipos de géneros
+    //Almacena el índice en el que se encuentra el valor de cada formato en el recurso arrays
+    public final int GENRE_ACTION = 0;
+    public final int GENRE_COMEDY = 1;
+    public final int GENRE_DRAMA = 2;
+    public final int GENRE_SCIFI = 3;
+    public final int GENRE_HORROR = 4;
+
+    //Propiedades de la clase
+    private int imageResId;
+    private String title;
+    private String director;
+    private int year;
+    private int genre;
+    private int format;
+    private String imdbURL;
+    private String comments;
+
+    //Constructor por defecto
+    public Film() {
+    }
+
+    //Constructor por parámetros
+    public Film(int imageResId, String title, String director, int year, int genre, int format, String imdbURL, String comments) {
+        this.imageResId = imageResId;
+        this.title = title;
+        this.director = director;
+        this.year = year;
+        this.genre = genre;
+        this.format = format;
+        this.imdbURL = imdbURL;
+        this.comments = comments;
+    }
+
+    //Getters y setters
+    public int getGENRE_ACTION() {
+        return GENRE_ACTION;
+    }
+
+    public int getGENRE_COMEDY() {
+        return GENRE_COMEDY;
+    }
+
+    public int getGENRE_DRAMA() {
+        return GENRE_DRAMA;
+    }
+
+    public int getGENRE_SCIFI() {
+        return GENRE_SCIFI;
+    }
+
+    public int getGENRE_HORROR() {
+        return GENRE_HORROR;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public void setImageResId(int imageResId) {
+        this.imageResId = imageResId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getGenre() {
+        return genre;
+    }
+
+    public void setGenre(int genre) {
+        this.genre = genre;
+    }
+
+    public int getFormat() {
+        return format;
+    }
+
+    public void setFormat(int format) {
+        this.format = format;
+    }
+
+    public String getImdbURL() {
+        return imdbURL;
+    }
+
+    public void setImdbURL(String imdbURL) {
+        this.imdbURL = imdbURL;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    //Método para imprimir por pantalla
+    @Override
+    public String toString() {
+        return "Film{" +
+                "GENRE_ACTION=" + GENRE_ACTION +
+                ", GENRE_COMEDY=" + GENRE_COMEDY +
+                ", GENRE_DRAMA=" + GENRE_DRAMA +
+                ", GENRE_SCIFI=" + GENRE_SCIFI +
+                ", GENRE_HORROR=" + GENRE_HORROR +
+                ", imageResId=" + imageResId +
+                ", title='" + title + '\'' +
+                ", director='" + director + '\'' +
+                ", year=" + year +
+                ", genre=" + genre +
+                ", format=" + format +
+                ", imdbURL='" + imdbURL + '\'' +
+                ", comments='" + comments + '\'' +
+                '}';
+    }
+}
